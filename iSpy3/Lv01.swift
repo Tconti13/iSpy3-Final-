782177190fc0ae6badd081e75b7bb0166ccafabe
Tconti <student@tconti.local>
2016-12-08 12:28:58 -0600

    Second test proved success. Outlet two connected without error.

--- iSpy3/Lv01.swift
+++ iSpy3/Lv01.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class Lv01: UIViewController {
+    //The item List (Deer, Duck, Seagull, Chicken, Padlock, Octopus)
+    
     @IBOutlet weak var deerLevel01: UILabel!
+    @IBOutlet weak var duckLevel01: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
