31c04e8a6107b265084992c961991b93f72a7bd6
Tconti <student@tconti.local>
2016-12-08 12:17:28 -0600

    Initial Commit

---
+++ iSpy3/Lv01.swift
@@ -0,0 +1,23 @@
+//
+//  Lv01.swift
+//  iSpy3
+//
+//  Created by Tconti on 12/8/16.
+//  Copyright © 2016 Tconti. All rights reserved.
+//
+
+import UIKit
+
+class Lv01: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBOutlet weak var listDeer01: UILabel!
+    
+    
+    
+    
+    
+}
