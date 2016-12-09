94c24ace6353507870ef2ed6d1a6feb7d9abf4f5
Tconti <student@tconti.local>
2016-12-09 12:05:07 -0600

    Front page constraints + beautification

--- iSpy3/Level01SpyLabels.swift
+++ iSpy3/Level01SpyLabels.swift
@@ -14,6 +14,7 @@ import UIKit
 
 
 class Level01SpyLabels: UILabel {
+    var canTap = true
 
 
     
--- iSpy3/Lv01.swift
+++ iSpy3/Lv01.swift
@@ -28,24 +28,28 @@ class Lv01: UIViewController {
     @IBOutlet weak var spyOctopus01: Level01SpyLabels!
     @IBOutlet weak var spyPadLock01: Level01SpyLabels!
     @IBOutlet weak var spyChicken01: Level01SpyLabels!
-    
-    
-
-    
-    
-    
+    var spyArray = [Level01SpyLabels]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        spyArray.append(spyDeer01)
+        spyArray.append(spySeagull01)
+        spyArray.append(spyDuck01)
+        spyArray.append(spyOctopus01)
+        spyArray.append(spyPadLock01)
+        spyArray.append(spyChicken01)
     }
     @IBAction func onLevel01Tapped(_ sender: UITapGestureRecognizer) {
         print("success")
-    }
-    
-    
-    
-    
-    
+        for item in spyArray{
+            if item.frame.contains(sender.location(in: backgroundLevel01 )){
+                if item.canTap {
+                    item.backgroundColor = UIColor.green
+                    print("object found")
+                    //when you want to add a scoreboard. Each point will be deducted if canTap equals false. Accomadate for that
+}
+}
+}
+}
 }
