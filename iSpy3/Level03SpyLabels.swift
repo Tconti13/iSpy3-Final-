f61d7194e2d09fcfae1c6de93c669b4db34d82cf
Tconti <student@tconti.local>
2016-12-11 21:44:40 -0600

    Level 3 complete. Finishing touches.

---
+++ iSpy3/Level03SpyLabels.swift
@@ -0,0 +1,14 @@
+//
+//  Level03SpyLabels.swift
+//  iSpy3
+//
+//  Created by Tconti on 12/11/16.
+//  Copyright © 2016 Tconti. All rights reserved.
+//
+
+import UIKit
+
+class Level03SpyLabels: UILabel {
+var canTap = true
+
+}
